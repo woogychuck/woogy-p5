b5629fe72b8717d9f0df6fd347d761e3b751ca67
Ian Muir <ian.muir@meltwater.com>
2020-06-10 12:02:08 -0400

    Moving background call

--- example/example.js
+++ example/example.js
@@ -1,13 +1,12 @@
 //Called Once
 function setup() {
   createCanvas(1000, 800);
+  background("#fff");
   fill("#FC0");
   stroke("#C90");
 }
 
 //Called every frame
 function draw() {
-  background("#fff");
-
   ellipse(500, 400, 100, 100);
 }
